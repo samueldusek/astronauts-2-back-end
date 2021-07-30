dee3d0c140b266c6f8f54684a53ddc4c3a704238
samueldusek <smldsk@protonmail.com>
2021-07-30 16:11:13 +0200

    create add, delete and update routes

--- controllers/astronauts.js
+++ controllers/astronauts.js
@@ -1,6 +1,38 @@
 const Astronaut = require("../models/astronauts");
 
-module.exports.sendAllAstronauts = async (req, res) => {
+module.exports.getAllAstronauts = async (req, res) => {
   const astronauts = await Astronaut.find();
-  res.json(astronauts);
+  res.status(200).json(astronauts);
+};
+
+module.exports.getAstronaut = async (req, res) => {
+  const { astronautId } = req.params;
+  const astronaut = await Astronaut.findById(astronautId);
+  res.status(200).json(astronaut);
+};
+
+module.exports.addAstronaut = async (req, res) => {
+  const astronaut = req.body;
+  const newAstronaut = await new Astronaut(astronaut);
+  await newAstronaut.save();
+  res.status(201).json({ message: "New astronaut added." });
+};
+
+module.exports.updateAstronaut = async (req, res) => {
+  const { astronautId } = req.params;
+  const astronaut = req.body;
+  const updatedAstronaut = await Astronaut.findByIdAndUpdate(
+    astronautId,
+    astronaut,
+    {
+      useFindAndModify: false,
+    }
+  );
+  res.status(200).json({ message: "Your astronaut has been updated!" });
+};
+
+module.exports.deleteAstronaut = async (req, res) => {
+  const { astronautId } = req.params;
+  const deletedAstronaut = await Astronaut.findByIdAndDelete(astronautId);
+  res.status(200).json({ message: "You astronaut has been deleted!" });
 };
--- routes/astronauts.js
+++ routes/astronauts.js
@@ -2,6 +2,15 @@ const express = require("express");
 const router = express.Router();
 const astronautsController = require("../controllers/astronauts");
 
-router.route("/").get(astronautsController.sendAllAstronauts);
+router
+  .route("/")
+  .get(astronautsController.getAllAstronauts)
+  .post(astronautsController.addAstronaut);
+
+router
+  .route("/:astronautId")
+  .get(astronautsController.getAstronaut)
+  .put(astronautsController.updateAstronaut)
+  .delete(astronautsController.deleteAstronaut);
 
 module.exports = router;
